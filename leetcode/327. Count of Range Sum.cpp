3e5eb598a226c2f14920615edb6a6e4b678444ea
Utkarsh Kumar <utkarshkumar@Utkarshs-MacBook-Air.local>
2023-05-07 13:43:52 +0530

    segment tree updated

--- leetcode/327. Count of Range Sum.cpp
+++ leetcode/327. Count of Range Sum.cpp
@@ -12,60 +12,79 @@ using namespace std;
 #define mod 1000000007
 #define ll long long int
 
-class Solution {
+class segTree{
+    // segmenting should be done on the basis of index due to constraints. Each index has a given prefixSum
+    // starting from index n-1, I'll store its sum in the segTree. To store the sum, I need the segmentation
+    // on the basis of prefix sums. The info stored in the tree is the count of prefixSums which lie in the given
+    // range
+    ll lfIdxTree,rtIdxTree,infoStored;
+    segTree *lf,*rt;
+    void modifyInfo(ll valAr){
+        this->infoStored = (this->infoStored+valAr);
+    }
 
-struct SegmentTree {
-    SegmentTree* left=NULL;
-    SegmentTree* right=NULL;
-    int count;
-    long min,max;
-    
-    // ar is sorted
-    SegmentTree(vector<long>& ar, int l, int r):left(NULL),right(NULL) {
-        if (l>r) return;
-        min = ar[l];
-        max = ar[r];
-        count=0;
-       if (l==r) return;
-        int m = (l+r)/2;
-        if (l<=m) left = new SegmentTree(ar, l,m);
-        if (m+1<=r) right = new SegmentTree(ar, m+1, r);
+    void updateInfo(){
+        this->infoStored=0;
+        if(lf!=nullptr) this->infoStored += this->lf->infoStored;
+        if(rt!=nullptr) this->infoStored += this->rt->infoStored;
     }
-    
-    void add(long value) {
-        if (value<min || value>max) return;
-        count++;
-        if (left && value<=left->max) left->add(value);
-        else if (right && value>=right->min) right->add(value);
+    public:
+
+    segTree(ll lfIdxAr,ll rtIdxAr,vector<ll> &ar){
+        lf=nullptr;
+        rt=nullptr;
+        this->lfIdxTree = ar[lfIdxAr];
+        this->rtIdxTree = ar[rtIdxAr];
+        infoStored=0;
+        if(lfIdxAr==rtIdxAr) return;
+        ll mid=lfIdxAr+(rtIdxAr-lfIdxAr)/2;
+        if(mid>=lfIdxAr) this->lf = new segTree(lfIdxAr,mid,ar);
+        if(mid+1<=rtIdxAr) this->rt = new segTree(mid+1,rtIdxAr,ar);
     }
-    
-    int getCount(long minValue, long maxValue) {
-        if (minValue<=min && max<=maxValue) return count;
-        if (maxValue<min || minValue>max) return 0;
-        int leftCount = left ? left->getCount(minValue, maxValue) : 0;
-        int rightCount = right ? right->getCount(minValue, maxValue) : 0;
-        return leftCount+rightCount;
+
+
+    void update(ll idxAr,ll valAr){
+        if(idxAr<lfIdxTree || idxAr>rtIdxTree) return;
+        // should I go left or  right? idxAr will tell
+        if(((this->lf)!=nullptr) && idxAr<=(this->lf->rtIdxTree)){
+            this->lf->update(idxAr,valAr);
+        }
+        else if((this->rt!=nullptr) && idxAr>=(this->rt->lfIdxTree)){
+            this->rt->update(idxAr,valAr);
+        }
+        // updateInfo();    
+        modifyInfo(valAr);
     }
+
+    ll getInfo(ll lfIdxAr,ll rtIdxAr){
+        if(lfIdxTree>=lfIdxAr && rtIdxTree<=rtIdxAr){
+            return this->infoStored;
+        }
+        if(lfIdxAr>rtIdxTree || rtIdxAr<lfIdxTree) return 0;
+        ll curAns=0;
+        if(lf!=nullptr) curAns+=this->lf->getInfo(lfIdxAr,rtIdxAr);
+        if(rt!=nullptr) curAns+=this->rt->getInfo(lfIdxAr,rtIdxAr);
+        return curAns;
+    }
+
 };
-    
-    
+
+class Solution {
 public:
     int countRangeSum(vector<int>& ar, int lower, int upper) {
-        if (ar.size()==0) return 0;
-        long sum=0;
-        vector<long> sums;
-        for(int i=0;i<ar.size();i++){
-            sum+=ar[i];
-            sums.push_back(sum);
+        if(ar.size()==0) return 0;
+        vector<ll> pSumsSorted;
+        pSumsSorted.push_back(ar[0]);
+        for(ll i=1;i<ar.size();i++) pSumsSorted.push_back(pSumsSorted.back()+ar[i]);
+        ll totSum=pSumsSorted.back(),ans=0;
+        sort(pSumsSorted.begin(),pSumsSorted.end());
+        segTree tree(0,pSumsSorted.size()-1,pSumsSorted);
+        for(ll i=ar.size()-1;i>=0;i--){
+            ans+=tree.getInfo(lower+totSum,upper+totSum);
+            tree.update(totSum,1);
+            if(totSum>=lower && totSum<=upper) ans++;
+            totSum-=ar[i];
         }
-        sort(sums.begin(), sums.end());
-        SegmentTree* tree = new SegmentTree(sums, 0, sums.size()-1);
-        int count = 0;
-        for(int i=ar.size()-1;i>=0;i--) {
-            tree->add(sum);
-            sum-=ar[i];
-            count += tree->getCount(lower+sum, upper+sum);
-        }        
-        return count;
+        return ans;
     }
 };
