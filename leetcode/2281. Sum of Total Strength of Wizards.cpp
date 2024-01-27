bb662f33703f5666de53b07297e33d9d2f3aae02
Utkarsh Kumar <utkarshkumar@Utkarshs-MacBook-Air.local>
2024-01-27 21:09:33 +0530

    prefix sum calc handled

--- leetcode/2281. Sum of Total Strength of Wizards.cpp
+++ leetcode/2281. Sum of Total Strength of Wizards.cpp
@@ -1,10 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 #define pb push_back
 #define mii map<int, int>
 #define mll map<ll, ll>
-#define pii pair<int, int>
 #define pic pair<int, char>
 #define pdd pair<double, double>
 #define pll pair<ll, ll>
@@ -12,30 +10,58 @@ using namespace std;
 #define ull unsigned long long int
 #define mod 1000000007
 #define ll long long int
+#define pii pair<int, int>
 
 class Solution {
 public:
     int totalStrength(vector<int>& ar) {
-        ll n=(ll)ar.size(),ans=0;
-        stack<ll> stk;
-        vector<ll> leftSmall(n,-1),rightSmall(n,n),ps(n,ar[0]),pps(n,ar[0]);
-        for(ll i=0;i<n;i++){
-            if(i){
-                ps[i]=ps[i-1]+ar[i];
-                pps[i]=pps[i-1]+ps[i];
-            }
-            while (stk.size() && ar[stk.top()]>=ar[i]){
-                rightSmall[stk.top()]=i;
+        int n=ar.size(),ans=0;
+        vector<int> sumEndingAtI(n,ar[0]),pSum(n,ar[0]);
+        for(int i=1;i<n;i++){
+            pSum[i]=pSum[i-1]+ar[i];
+            sumEndingAtI[i]=sumEndingAtI[i-1]+ar[i]*(i+1);
+        }
+        vector<pii> bdr(n);
+        stack<int> stk;
+        for(int i=0;i<n;i++){
+            while(stk.size() && ar[stk.top()]>ar[i]){
                 stk.pop();
             }
-            if(stk.size()) leftSmall[i]=stk.top();
+            bdr[i].first = (stk.size())?stk.top():-1;
             stk.push(i);
-        }  
-        for(ll i=0;i<n;i++){
-            ll inc = (((pps[rightSmall[i]-1]-pps[i]+ps[i])%mod*(i-leftSmall[i])%mod) -
-                      ((pps[i]-pps[leftSmall[i]+1]+ps[leftSmall[i]+1])%mod*(rightSmall[i]-i)%mod) +
-                      ((ps[i]-ps[leftSmall[i]+1]+ar[leftSmall[i]+1])%mod*(rightSmall[i]-i)%mod));
-            ans=(ans+inc%mod*ar[i]%mod+mod)%mod;
+        }
+        while (stk.size()) stk.pop();
+        for(int i=n-1;i>=0;i--){
+            while (stk.size() && ar[stk.top()]>=ar[i]){
+                stk.pop();
+            }
+            bdr[i].second = (stk.size())?stk.top():n;
+            stk.push(i);
+        }
+        //  debug
+        for(auto it:bdr){
+            cout<<"{"<<it.first<<" - "<<it.second<<"} ";
+        }
+        cout<<endl;
+        //  debug
+
+        //  debug
+        for(auto it:sumEndingAtI){
+            cout<<"{"<<it<<"} ";
+        }
+        cout<<endl;
+        //  debug
+
+        for(int i=0;i<n;i++){
+            int lfSum1 = (bdr[i].first==-1)?0:sumEndingAtI[bdr[i].first];
+            int sum1 = sumEndingAtI[i]-lfSum1;
+            cout<<"sum1 : "<<sum1<<endl;
+            int lfSum2 = (bdr[i].first==-1)?0:pSum[bdr[i].first];
+            int factor = bdr[i].first+1;
+            cout<<"factor : "<<factor<<endl;
+            int sum2 = (pSum[i]-lfSum2)*factor;
+            cout<<"sum2 : "<<sum2<<endl;
+            cout<<"Required sum of all subarrays ending at "<<i<<" = "<<sum1-sum2<<endl;
         }
         return ans;
     }
