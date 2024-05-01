3d55e1918df8bb3eaaf8295a1c2a138b733f7ec8
Utkarsh Kumar <utkarshkumar@Utkarshs-MacBook-Air.local>
2024-05-02 01:39:13 +0530

    p1

--- leetcode/134. Gas Station.cpp
+++ leetcode/134. Gas Station.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 #define pb push_back
 #define mii map<int, int>
 #define mll map<ll, ll>
@@ -13,37 +12,33 @@ using namespace std;
 #define mod 1000000007
 #define ll long long int
 
-// 9:11
-
 class Solution {
 public:
-    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int n=gas.size(),idx=0,len=0,sum=0;
-        for(int i=0;i<n;i++) gas[i]=gas[i]-cost[i];
-        for(int i=0;i<2*n;i++){
-            idx=i%n;
-            sum+=gas[idx];
-            if(sum<0){
+    int canCompleteCircuit(vector<int>& ar, vector<int>& cost) {
+        int n=ar.size();
+        if(n<=1) return ar[0]>=cost[0]?0:-1;
+        vector<int> del(n,0);
+        for(int i=0;i<n;i++){
+            del[i] = ar[i]-cost[i];
+        }
+
+        int ptr=-1, sum=0;
+
+        for(int it=0;it<2*n;it++){
+            int idx = it%n;
+            sum+=del[idx];
+            if(sum>0){
+                if(ptr==-1){
+                    ptr=idx;
+                }
+            }else if(sum<0){
+                ptr=-1;
                 sum=0;
-                len=0;
-            }else{
-                len++;
-                if(len==n) return (idx+1)%n;
+            }
+            if(sum>=0 && (it+1+n)%n==ptr){
+                return ptr;
             }
         }
         return -1;
     }
 };
-
-/*
-Input: gas = [1,2,3,4,5], cost = [3,4,5,1,2]
-Output: 3
-Explanation:
-Start at station 3 (index 3) and fill up with 4 unit of gas. Your tank = 0 + 4 = 4
-Travel to station 4. Your tank = 4 - 1 + 5 = 8
-Travel to station 0. Your tank = 8 - 2 + 1 = 7
-Travel to station 1. Your tank = 7 - 3 + 2 = 6
-Travel to station 2. Your tank = 6 - 4 + 3 = 5
-Travel to station 3. The cost is 5. Your gas is just enough to travel back to station 3.
-Therefore, return 3 as the starting index.
-*/
