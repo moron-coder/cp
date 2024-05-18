5633eff01db0ee198141c5fa72db2589cb25564b
Utkarsh Kumar <utkarshkumar@Utkarshs-MacBook-Air.local>
2024-05-18 15:48:13 +0530

    bellman ford

--- leetcode/743. Network Delay Time.cpp
+++ leetcode/743. Network Delay Time.cpp
@@ -3,6 +3,7 @@ using namespace std;
 #define pb push_back
 #define mii map<int, int>
 #define mll map<ll, ll>
+#define pii pair<int, int>
 #define pic pair<int, char>
 #define pdd pair<double, double>
 #define pll pair<ll, ll>
@@ -10,28 +11,25 @@ using namespace std;
 #define ull unsigned long long int
 #define mod 1000000007
 #define ll long long int
-#define pii pair<int, int>
 
 class Solution {
+    const int MAX_WT = 10000000;
 public:
     int networkDelayTime(vector<vector<int>>& ar, int n, int k) {
-        int ans=-1;
-        vector<vector<pii>> edges(n,vector<pii>());
-        for(auto it:ar) edges[it[0]].push_back({it[1]-1,it[2]});  //  {idx,dis}
-        bool *vis=new bool[n]();
-        k--;
-        priority_queue<pii,vector<pii>,greater<pii>> pq;    //  {dis,idx}
-        pq.push({0,k-1});
-        while(pq.size()){
-            pii tp=pq.top();
-            pq.pop();
-            if(vis[tp.second]) continue;
-            cout<<tp.second<<" vis\n";
-            vis[tp.second]=true;
-            ans=max(ans,tp.first);
-            for(auto it:edges[tp.second]) if(!vis[it.first]) pq.push({tp.first+it.second,it.first});
+        vector<int> wt(n,MAX_WT);
+        wt[k-1]=0;
+        for(int ct=0;ct<n-1;ct++){
+            for(int i=0;i<ar.size();i++){
+                int u=ar[i][0]-1, v=ar[i][1]-1, w=ar[i][2];
+                if(wt[u]!=MAX_WT){
+                    wt[v]=min(wt[v], wt[u]+w);
+                }
+            }
         }
-        for(int i=0;i<n;i++) if(!vis[i]) return -1;
-        return ans;
+        int ans=0;
+        for(auto it:wt){
+            ans=max(ans,it);
+        }
+        return (ans==MAX_WT)?-1:ans;
     }
 };
