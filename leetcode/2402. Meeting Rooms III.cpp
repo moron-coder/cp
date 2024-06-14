493a675bf908260cf19394de9d30f2ce68416d79
Utkarsh Kumar <utkarshkumar@Utkarshs-MacBook-Air.local>
2024-06-14 09:04:55 +0530

    practice

--- leetcode/2402. Meeting Rooms III.cpp
+++ leetcode/2402. Meeting Rooms III.cpp
@@ -1,61 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define pll pair<ll, ll>
+
 #define ll long long int
+#define pll pair<ll, ll>
 
 class Solution {
-public:
-    map<int,int> mp;
+    void emptyRooms(priority_queue<ll,vector<ll>,greater<ll>> &freeRooms,
+                priority_queue<pll,vector<pll>,greater<pll>> &busyRooms,
+                ll timer
+    ){
+        while (busyRooms.size() && busyRooms.top().first<=timer){
+            pll cur = busyRooms.top();
+            busyRooms.pop();
 
-    void allotRoom(set<int> &freeRooms,priority_queue<pll,vector<pll>,greater<pll>> &rooms_busy,ll &time,vector<int> &meeting){
-
-
-        if(freeRooms.size()){
-            // allot room
-            int roomId = *freeRooms.begin();
-            freeRooms.erase(roomId);
-            rooms_busy.push({time+meeting[1]-meeting[0],roomId});
-            mp[roomId]++;
-        }else{
-            // wait for a room to be free
-            time=rooms_busy.top().first;
-            while (rooms_busy.size() && rooms_busy.top().first<=time){
-                pll tp = rooms_busy.top();
-                rooms_busy.pop();
-                freeRooms.insert(tp.second);
-            }
-            allotRoom(freeRooms,rooms_busy,time,meeting);
+            freeRooms.push(cur.second);
         }
     }
 
-    int mostBooked(int n, vector<vector<int>>& meetings) {
-        mp.clear();
-        ll time=0;
-        set<int> freeRooms;
-        for(int i=0;i<n;i++) freeRooms.insert(i);
-        priority_queue<pll,vector<pll>,greater<pll>> rooms_busy;        //  {end_time, room_id}
-        sort(meetings.begin(),meetings.end());
-        for(auto it:meetings){
-            if(it[0]<=time){
-                allotRoom(freeRooms,rooms_busy,time,it);
-            }else{
-                // increase time
-                time=it[0];
-                while (rooms_busy.size() && rooms_busy.top().first<=time){
-                    pll tp = rooms_busy.top();
-                    rooms_busy.pop();
-                    freeRooms.insert(tp.second);
-                }
-                allotRoom(freeRooms,rooms_busy,time,it);
+public:
+    int mostBooked(int n, vector<vector<int>>& ar) {
+        priority_queue<ll,vector<ll>,greater<ll>> freeRooms;     //  {room_id}
+        priority_queue<pll,vector<pll>,greater<pll>> busyRooms;     //  {meeting_end_time, room_id}
+        for(ll i=0;i<n;i++) freeRooms.push(i);
+        ll *meetingsCount = new ll[n]();
+        ll timer=0;
+        sort(ar.begin(), ar.end());
+
+        for(auto it:ar){
+            timer = max(timer, (ll)it[0]);
+            emptyRooms(freeRooms, busyRooms, timer);
+            
+            if(freeRooms.empty()){
+                timer = busyRooms.top().first;
+                emptyRooms(freeRooms, busyRooms, timer);
+            }
+            ll curRoom = freeRooms.top();
+            freeRooms.pop();
+
+            busyRooms.push({timer+it[1]-it[0], curRoom});
+            meetingsCount[curRoom]++;
+        }
+        ll ans=0,mxMeetingCt=0;
+        for(ll i=0;i<n;i++){
+            if(meetingsCount[i]>mxMeetingCt){
+                mxMeetingCt = meetingsCount[i];
+                ans=i;
             }
         }
-        int maxFq=0,maxFqRoom=-1;
-        for(auto it:mp){
-            if(it.second>maxFq){
-                maxFqRoom=it.first;
-                maxFq=it.second;
-            }
-        }
-        return maxFqRoom;
+        return ans;
     }
 };
